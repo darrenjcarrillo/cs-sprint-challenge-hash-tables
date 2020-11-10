9b41ab5df864eddf1020c78e782e24e159192455
Darren Carrillo <48144231+darrenjcarrillo@users.noreply.github.com>
2020-11-10 13:23:07 -0800

    ex1 completed

--- hashtables/ex1/ex1.py
+++ hashtables/ex1/ex1.py
@@ -1,7 +1,23 @@
 def get_indices_of_item_weights(weights, length, limit):
-    """
-    YOUR CODE HERE
-    """
-    # Your code here
+    hash_table = {}
+
+    for i in range(length):
+        bucket = []
+        cur_weight = weights[i]
+        value = hash_table.get(limit-cur_weight)
+
+        if value is None:
+            hash_table[cur_weight] = i
+        else:
+            if value > i:
+                bucket.append(value)
+                bucker.append(i)
+            else:
+                bucket.append(i)
+                bucket.append(value)
+            return bucket
 
     return None
+
+
+print(get_indices_of_item_weights([4, 6, 10, 15, 16], 5, 21))
